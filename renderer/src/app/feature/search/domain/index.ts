74c82e4c8e652e9de98dca89ecc94674400bfa2e
Solgio <lorenzosoligo04@gmail.com>
2026-03-20 10:02:43 +0000

    DIPR-346 #comment Partial filters

---
+++ renderer/src/app/feature/search/domain/index.ts
@@ -0,0 +1,6 @@
+export type { AggregateFilterValues } from './search-aggregate-filters-models';
+export type { CommonFilterValues } from './search-common-filters-models';
+export type { CustomFilterValues } from './search-custom-filters-models';
+export type { DiDaiFilterValues } from './search-diDai-filters-models';
+export type { SubjectCriteria } from './search-subject-filters-models';
+export type { PartialFilters } from './partial-filters-models';
