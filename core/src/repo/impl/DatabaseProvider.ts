d3cc1c7b0e72775e4dcdb2c9ca522c748b01495b
zakka00 <zaka.laoud.2004@gmail.com>
2026-03-30 14:45:43 +0200

    Minor Fix per corretta compilazione

--- core/src/repo/impl/DatabaseProvider.ts
+++ core/src/repo/impl/DatabaseProvider.ts
@@ -9,16 +9,23 @@ import Database from 'better-sqlite3';
 import * as path from 'node:path';
 import * as os from 'node:os';
 import * as fs from 'node:fs';
-import * as sqliteVss from 'sqlite-vss';
 
 export const DATABASE_PROVIDER_TOKEN = Symbol('DatabaseProvider');
 
 @injectable()
 export class DatabaseProvider {
     private _db: Database.Database | null = null;
-
+    private _initPromise: Promise<Database.Database> | null = null;
     public get db(): Database.Database {
         if (!this._db) {
+            throw new Error('DatabaseProvider non inizializzato. Chiama await dbProvider.init() prima.');
+        }
+        return this._db;
+    }
+    public init(): Promise<Database.Database> {
+        if (this._initPromise) return this._initPromise;
+
+        this._initPromise = (async () => {
             const dir = path.join(os.homedir(), '.dip-viewer');
             fs.mkdirSync(dir, { recursive: true });
             const dbPath = path.join(dir, 'dip-viewer.db');
@@ -26,8 +33,11 @@ export class DatabaseProvider {
             this._db = new Database(dbPath);
             this._db.pragma('journal_mode = WAL');
             this._db.pragma('foreign_keys = ON');
+            const sqliteVss = await (new Function('return import("sqlite-vss")'))();
             sqliteVss.load(this._db);
-        }
-        return this._db;
+            return this._db;
+        })();
+
+        return this._initPromise;
     }
 }
