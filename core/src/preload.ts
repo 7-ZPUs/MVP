1f75b5ab5692fe2ce04f5be5afdc7bf21a877241
zakka00 <zaka.laoud.2004@gmail.com>
2026-04-04 12:11:29 +0200

    Aggiunta channels

--- core/src/preload.ts
+++ core/src/preload.ts
@@ -1,29 +1,50 @@
 import { contextBridge, ipcRenderer } from "electron";
 
+const validChannels = new Set([
+  "package:open",
+  "package:list",
+  "package:close",
+  "ipc:search:text",
+  "ipc:search:semantic",
+  "ipc:search:advanced",
+  "ipc:indexing:status",
+  "integrity:verify",
+  "check-integrity:document",
+  "check-integrity:file",
+  "check-integrity:process",
+  "check-integrity:document-class",
+  "check-integrity:dip",
+  "create:create-document",
+  "create:create-process",
+  "create:create-file",
+  "create:create-document-class",
+  "create:create-dip",
+  "browse:get-document-by-id",
+  "browse:get-documents-by-process",
+  "browse:get-documents-by-status",
+  "browse:get-file-by-id",
+  "browse:get-file-buffer-by-id",
+  "browse:get-file-by-document",
+  "browse:get-file-by-status",
+  "browse:get-process-by-id",
+  "browse:get-process-by-status",
+  "browse:get-process-by-document-class",
+  "browse:get-document-class-by-dip-id",
+  "browse:get-document-class-by-status",
+  "browse:get-document-class-by-id",
+  "browse:get-dip-by-id",
+  "browse:get-dip-by-status",
+  "browse:get-dip-by-document-class",
+  "file:open-external",
+  "file:download",
+  "file:save-dialog",
+]);
+
 contextBridge.exposeInMainWorld("electronAPI", {
   invoke: (channel: string, data: any) => {
-    const validChannels = [
-      "ipc:search:text",
-      "ipc:search:semantic",
-      "ipc:search:advanced",
-      "ipc:indexing:status",
-      "file:open-external",
-      "file:download",
-      "file:save-dialog",
-      "browse:get-dip-by-id",
-      "browse:get-class-by-dip-id",
-      
-
-    ];
-
-    const isValid = validChannels.includes(channel);
-
-    if (isValid) {
+    if (validChannels.has(channel)) {
       return ipcRenderer.invoke(channel, data);
-    } else {
-      return Promise.reject(
-        new Error(`Canale IPC non autorizzato: ${channel}`),
-      );
     }
+    return Promise.reject(new Error(`Canale IPC non autorizzato: ${channel}`));
   },
 });
